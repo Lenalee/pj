dfdfb01425851958228c3f1addbd3fd5fa751793
Yana <zajchig@gmail.com>
2017-08-28 03:06:39 +0200

    initial

---
+++ src/components/Button.vue
@@ -0,0 +1,17 @@
+<template>
+<div>
+<button class="btn btn-primary" >{{ name }}</button>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'button',
+  data () {
+    return {}
+  },
+  props: [
+    'name'
+  ]
+}
+</script>
---
+++ src/components/Hello.vue
@@ -0,0 +1,19 @@
+<template>
+<div>
+<b-tn name="blb" ></b-tn>
+</div>
+</template>
+
+<script>
+import Button from "@/components/Button"
+
+export default {
+  name: 'hello',
+  data () {
+    return {}
+  },
+  components: {
+    "b-tn": Button
+  }
+}
+</script>
